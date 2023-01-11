a69b3f721b603f338d77f363f7922f9620f88c14
Yannick Flores <yannick.flores1992@gmail.com>
2023-01-11 10:46:45 +0100

    Replace circleci by github actions workflows

--- ipfy_asset_management_pdf_to_table/_textract_zip_process.py
+++ ipfy_asset_management_pdf_to_table/_textract_zip_process.py
@@ -14,18 +14,24 @@ class TextractZipProcessResult(pd.DataFrame):
 class TextractZipToDataFrame:
         
     def unzip_textract_zip_file(
-        #grid_data_list: dict
+        path_to_zip_file: str,
+        directory_to_extract_to: str
         ) -> pd.DataFrame:
         """
-        Process and tranform to pd.DataFrame needed data from zip 
+        Process and tranform data in Zip to pd.DataFrame
         Zip file is obtained from a manual process of using AWS textract services
         This class worked only for a specifc type of docuements from a specific bank
         """
 
-        # initialize list elements
-        data = [10,20,30,40,50,60]
-        
-        # Create the pandas DataFrame with column name is provided explicitly
-        df = pd.DataFrame(data, columns=['Numbers'])
+        with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+            zip_ref.extractall(directory_to_extract_to)
 
-        return df
+        list_unzipped_files = os.listdir(directory_to_extract_to)
+
+        # # initialize list elements
+        # data = [10,20,30,40,50,60]
+        
+        # # Create the pandas DataFrame with column name is provided explicitly
+        # df = pd.DataFrame(data, columns=['Numbers'])
+
+        return list_unzipped_files
