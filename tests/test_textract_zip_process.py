5fb7f89466e8d27ac3bfbbf60cfac8e46571f6df
Yannick Flores <yannick.flores1992@gmail.com>
2023-01-13 11:41:17 +0100

    version 0.2.5 - Added a new example for test

--- tests/test_textract_zip_process.py
+++ tests/test_textract_zip_process.py
@@ -5,12 +5,25 @@ from ipfy_asset_management_pdf_to_table import (
     TextractZipToDataFrame
 )
 
-def test_textract_zip_process():
+def test_textract_zip_process_exemple_1():
 
     textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
 
-    list_file_unzipped = textract_zip.unzip_textract_zip_file(
-        path_to_zip_file = './tests/data_example/example.zip'
+    list_file_unzipped= textract_zip.unzip_textract_zip_file(
+        path_to_zip_file = './tests/data_example/example_1.zip'
+    )
+
+    unifed_dataframe = textract_zip.get_csv_table_from_unzipped_file()
+
+    assert len(list_file_unzipped) > 0
+    assert unifed_dataframe is not None
+
+def test_textract_zip_process_exemple_2():
+
+    textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
+
+    list_file_unzipped= textract_zip.unzip_textract_zip_file(
+        path_to_zip_file = './tests/data_example/example_2.zip'
     )
 
     unifed_dataframe = textract_zip.get_csv_table_from_unzipped_file()
