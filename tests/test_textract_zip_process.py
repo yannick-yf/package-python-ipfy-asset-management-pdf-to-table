20a6cec0282173c7b505962f71ea44511c276004
Yannick Flores <yannick.flores1992@gmail.com>
2023-01-19 11:05:30 +0100

    v 0.2.7
    Add CA Next bank file type
    Rename test function to English terms

--- tests/test_textract_zip_process.py
+++ tests/test_textract_zip_process.py
@@ -5,7 +5,7 @@ from ipfy_asset_management_pdf_to_table import (
     TextractZipToDataFrame
 )
 
-def test_textract_zip_process_exemple_1():
+def test_textract_zip_process_example_1():
 
     textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
 
@@ -13,12 +13,12 @@ def test_textract_zip_process_exemple_1():
         path_to_zip_file = './tests/data_example/example_1.zip'
     )
 
-    unifed_dataframe = textract_zip.get_csv_table_from_unzipped_file()
+    unified_dataframe = textract_zip.get_csv_table_from_unzipped_file()
 
     assert len(list_file_unzipped) > 0
-    assert unifed_dataframe is not None
+    assert unified_dataframe is not None
 
-def test_textract_zip_process_exemple_2():
+def test_textract_zip_process_example_2():
 
     textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
 
@@ -31,7 +31,20 @@ def test_textract_zip_process_exemple_2():
     assert len(list_file_unzipped) > 0
     assert unified_dataframe is not None
 
-def test_textract_zip_process_exemple_CA_CC():
+def test_textract_zip_process_example_CANEXT_CC():
+
+    textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
+
+    list_file_unzipped= textract_zip.unzip_textract_zip_file(
+        path_to_zip_file = './tests/data_example/example_CANEXT_CC.zip'
+    )
+
+    unified_dataframe = textract_zip.get_csv_table_from_unzipped_file()
+
+    assert len(list_file_unzipped) > 0
+    assert unified_dataframe is not None
+
+def test_textract_zip_process_example_CA_CC():
 
     textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
 
@@ -39,12 +52,12 @@ def test_textract_zip_process_exemple_CA_CC():
         path_to_zip_file = './tests/data_example/example_CA_CC.zip'
     )
 
-    unifed_dataframe = textract_zip.get_csv_table_from_unzipped_file()
+    unified_dataframe = textract_zip.get_csv_table_from_unzipped_file()
 
     assert len(list_file_unzipped) > 0
-    assert unifed_dataframe is not None
+    assert unified_dataframe is not None
 
-def test_textract_zip_process_exemple_CA_CEL():
+def test_textract_zip_process_example_CA_CEL():
 
     textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
 
@@ -57,7 +70,7 @@ def test_textract_zip_process_exemple_CA_CEL():
     assert len(list_file_unzipped) > 0
     assert unified_dataframe is not None
 
-def test_textract_zip_process_exemple_BB_CC():
+def test_textract_zip_process_example_BB_CC():
 
     textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
 
@@ -70,7 +83,7 @@ def test_textract_zip_process_exemple_BB_CC():
     assert len(list_file_unzipped) > 0
     assert unified_dataframe is not None
 
-def test_textract_zip_process_exemple_CA_LVTA():
+def test_textract_zip_process_example_CA_LVTA():
 
     textract_zip = TextractZipToDataFrame(directory_to_extract_to = './tests/data_example/unzipped')
 
